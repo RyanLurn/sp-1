72f3b4c27ac3dd06da87722d11d09b81b7123485
Ryan Luong <lumintriet@gmail.com>
2026-09-25 09:27:16 +0700

    fix(chat/schema): allow chat message metadata to be undefined.

--- src/features/chat/schemas.ts
+++ src/features/chat/schemas.ts
@@ -3,7 +3,7 @@ import type { UIMessage } from "ai";
 import { safeValidateUIMessages } from "ai";
 import { z } from "zod";
 
-export const ChatMessageMetadataSchema = z.json();
+export const ChatMessageMetadataSchema = z.union([z.undefined(), z.json()]);
 export type ChatMessageMetadata = z.infer<typeof ChatMessageMetadataSchema>;
 
 export type ChatMessage = UIMessage<ChatMessageMetadata, {}, {}>;
