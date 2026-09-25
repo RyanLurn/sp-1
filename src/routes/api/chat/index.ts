0e8033fbb9f684eec3f08bbcd6eaa25dc6d6c0e8
Ryan Luong <lumintriet@gmail.com>
2026-09-25 09:18:21 +0700

    feat(chat/api): parse and validate request body to get messages.

--- src/routes/api/chat/index.ts
+++ src/routes/api/chat/index.ts
@@ -1,5 +1,5 @@
-import type { UIMessage } from "ai";
-
+import { HTTP_ERROR_RESPONSE_STATUS_RECORD } from "@little-nebulae/http";
+import { parseJsonRequestBody } from "@little-nebulae/web-fetch";
 import { createFileRoute } from "@tanstack/react-router";
 import {
   convertToModelMessages,
@@ -7,18 +7,60 @@ import {
   streamText,
   toUIMessageStream,
 } from "ai";
+import { z } from "zod";
 
 import { groqProvider } from "@/config/ai/groq";
+import { safeValidateChatMessages } from "@/features/chat/schemas";
 
 export const Route = createFileRoute("/api/chat/")({
   server: {
     handlers: {
       POST: async ({ request }) => {
-        const { messages }: { messages: UIMessage[] } = await request.json();
+        // Parse and validate the request's body to get the messages array
+        const parseRequestBodyResult = await parseJsonRequestBody({ request });
+        if (!parseRequestBodyResult.success) {
+          const error = parseRequestBodyResult.error;
+          console.error(error);
+          const httpError = HTTP_ERROR_RESPONSE_STATUS_RECORD.BAD_REQUEST;
+          return new Response(error.message, {
+            status: httpError.code,
+            statusText: httpError.text,
+          });
+        }
+        const jsonBody = parseRequestBodyResult.data;
 
+        const parseJsonBodyResult = z
+          .object({ messages: z.unknown() })
+          .safeParse(jsonBody);
+        if (!parseJsonBodyResult.success) {
+          const error = parseJsonBodyResult.error;
+          console.error(error);
+          const httpError = HTTP_ERROR_RESPONSE_STATUS_RECORD.BAD_REQUEST;
+          return new Response(error.message, {
+            status: httpError.code,
+            statusText: httpError.text,
+          });
+        }
+        const { messages } = parseJsonBodyResult.data;
+
+        const validateMessagesResult = await safeValidateChatMessages({
+          messages,
+        });
+        if (!validateMessagesResult.success) {
+          const error = validateMessagesResult.error;
+          console.error(error);
+          const httpError = HTTP_ERROR_RESPONSE_STATUS_RECORD.BAD_REQUEST;
+          return new Response(error.message, {
+            status: httpError.code,
+            statusText: httpError.text,
+          });
+        }
+        const validMessages = validateMessagesResult.data;
+
+        // Stream AI response
         const { stream } = streamText({
           model: groqProvider("openai/gpt-oss-20b"),
-          messages: await convertToModelMessages(messages),
+          messages: await convertToModelMessages(validMessages),
         });
 
         return createUIMessageStreamResponse({
