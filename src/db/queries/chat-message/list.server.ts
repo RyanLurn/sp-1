d0d95bf3a3413a09cc238af4fd8d1425f3697d79
Ryan Luong <lumintriet@gmail.com>
2026-09-26 08:09:10 +0700

    feat(db/queries): define Result type for listChatMessages function.

--- src/db/queries/chat-message/list.server.ts
+++ src/db/queries/chat-message/list.server.ts
@@ -1,9 +1,30 @@
+import type { Result } from "@little-nebulae/result";
+
+import { composeErrorMessage, UnexpectedError } from "@little-nebulae/error";
+import { fail, succeed } from "@little-nebulae/result";
 import { createServerOnlyFn } from "@tanstack/react-start";
+
+import type { SelectedChatMessage } from "@/db/tables/chat-message.server";
 
 import { db } from "@/db";
 import { chatMessageTable } from "@/db/tables/chat-message.server";
 
-export const listChatMessages = createServerOnlyFn(async () => {
-  const selectedChatMessages = await db.select().from(chatMessageTable);
-  return selectedChatMessages;
-});
+export const listChatMessages = createServerOnlyFn(
+  async (): Promise<Result<SelectedChatMessage[], UnexpectedError>> => {
+    try {
+      const selectedChatMessages = await db.select().from(chatMessageTable);
+      return succeed(selectedChatMessages);
+    } catch (error) {
+      return fail(
+        new UnexpectedError({
+          message: composeErrorMessage({
+            operation: "list chat messages",
+            reason: "some unexpected error",
+          }),
+          cause: error,
+          meta: null,
+        }),
+      );
+    }
+  },
+);
