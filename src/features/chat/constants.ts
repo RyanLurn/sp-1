120a4916dc7d9944f53376cc0fd909dd108b2ee8
Ryan Luong <lumintriet@gmail.com>
2026-09-26 18:11:32 +0700

    refactor code.

--- src/features/chat/constants.ts
+++ src/features/chat/constants.ts
@@ -1,4 +1,11 @@
-export const CHAT_MESSAGE_ROLE_LIST = ["system", "user", "assistant"] as const;
+export const CHAT_MESSAGE_SYSTEM_ROLE = "system";
+export const CHAT_MESSAGE_USER_ROLE = "user";
+export const CHAT_MESSAGE_ASSISTANT_ROLE = "assistant";
+export const CHAT_MESSAGE_ROLE_LIST = [
+  CHAT_MESSAGE_SYSTEM_ROLE,
+  CHAT_MESSAGE_USER_ROLE,
+  CHAT_MESSAGE_ASSISTANT_ROLE,
+] as const;
 
 export const CHAT_MESSAGE_PART_STREAMING_STATE_LIST = [
   "streaming",
--- src/features/chat/schemas.ts
+++ src/features/chat/schemas.ts
@@ -1,13 +1,3 @@
-import type { OmitKnownKeys } from "@little-nebulae/type-utils";
-import type {
-  InferUITools,
-  TextUIPart,
-  ToolSet,
-  UIMessage,
-  UIMessagePart,
-} from "ai";
-
-import { safeValidateUIMessages } from "ai";
 import { z } from "zod";
 
 import {
@@ -38,36 +28,3 @@ export const ChatMessageReasoningPartSchema = z.object({
 export type ChatMessageReasoningPart = z.infer<
   typeof ChatMessageReasoningPartSchema
 >;
-
-export const ChatMessageMetadataSchema = z.json();
-export type ChatMessageMetadata = z.infer<typeof ChatMessageMetadataSchema>;
-
-export const ChatMessageDataPartSchema = z.object();
-export type ChatMessageDataPart = z.infer<typeof ChatMessageDataPartSchema>;
-
-export const tools = {} satisfies ToolSet;
-export type ChatMessageTools = InferUITools<typeof tools>;
-
-export type ChatMessagePart = Extract<
-  UIMessagePart<ChatMessageDataPart, ChatMessageTools>,
-  TextUIPart
->;
-
-export type ChatMessage = OmitKnownKeys<
-  UIMessage<ChatMessageMetadata, ChatMessageDataPart, ChatMessageTools>,
-  "parts"
-> & {
-  parts: ChatMessagePart[];
-};
-
-export async function safeValidateChatMessages({
-  messages,
-}: {
-  messages: unknown;
-}) {
-  return safeValidateUIMessages<ChatMessage>({
-    messages,
-    metadataSchema: ChatMessageMetadataSchema.optional(),
-    tools,
-  });
-}
